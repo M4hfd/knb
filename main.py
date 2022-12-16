db8cd4bc339cc402762f7a3971f37540fd8c66b5
M4hfd <83091151+M4hfd@users.noreply.github.com>
2022-12-16 15:30:41 +0300

    knb

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+count = 0
+for player in range(10):
+    if count == 3:
+       print("Победа!") 
+       break
+    player = int(input("Сделайте ход: 1 Камень, 2 ножницы, 3 бумага: "))		
+    if player == 1:
+        print("Вы выбрали камень")
+    if player == 2:
+        print("Вы выбрали ножницы")
+    if player == 3:
+        print("Вы выбрали бумагу")
+    comp = random.randint(1,3)
+
+    if comp == 1:
+        print("Ход оппонента - камень")
+    if comp == 2:
+        print("Ход оппонента - ножницы")
+    if comp == 3:
+        print("Ход оппонента - бумага")
+
+    if comp == 1 and player == 1:
+        print("Ничья!")
+        print("Твой счет: " + str(count))
+    if comp == 2 and player == 2:
+        print("Ничья!")
+        print("Твой счет: " + str(count))
+    if comp == 3 and player == 3:
+        print("Ничья!")
+        print("Твой счет: " + str(count))
+    if comp == 1 and player == 2:
+        print("Вы проиграли!")
+        print("Твой счет: " + str(count))
+    if comp == 2 and player == 1:
+        print("Вы выйграли!")
+        count += 1
+        print("Твой счет: " + str(count))
+    if comp == 3 and player == 1:
+        print("Вы выйграли!")
+        count += 1
+        print("Твой счет: " + str(count))
+    if comp == 1 and player == 3:
+        print("Вы проиграли!")
+        print("Твой счет: " + str(count))
+    if comp == 2 and player == 3:
+        print("Вы проиграли!")
+        print("Твой счет: " + str(count))
+    if comp == 3 and player == 2:
+        print("Вы выйграли!")
+        count += 1
+        print("Твой счет: " + str(count))
